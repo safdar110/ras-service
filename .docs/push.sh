753dc4c7102976432fd2714acf820287922ace37
Khawar <khawarhere@gmail.com>
2019-02-05 22:52:30 +0500

    updated via script

--- .docs/push.sh
+++ .docs/push.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
+
 export SERVICE_NAME=raservice
-#./mvnw package
-./mvnw -DskipTests package
-docker login -u gitlab-ci-token -p zVTStG4zXy8fseKHzD8c registry.gitlab.com
+
+eval $(minikube docker-env)
+
+./mvnw package
+
 git add .
 git commit -m "updated via script"
 git push origin master
 export VERSION=$(git rev-parse --short HEAD)
-docker build -t registry.gitlab.com/resourceallocationsystem/$SERVICE_NAME:$VERSION .
-docker push registry.gitlab.com/resourceallocationsystem/$SERVICE_NAME:$VERSION
-cat .docs/deployment.yaml | sed -e "s/@KVERSION/$VERSION/g" | kubectl apply -f-
+
+docker build -t $SERVICE_NAME:$VERSION .
+
+#cat .docs/configmap.yaml | kubectl apply -f-
+
+kubectl apply -f .docs/configmap.yaml
+
+cat .docs/deployment.yaml | sed -e "s/@KVERSION/$VERSION/g" -e "s/@KSERVICE/$SERVICE_NAME/g" | kubectl apply -f-
+
+#cat .docs/deployment-backup.yaml | sed -e "s/@KVERSION/$VERSION/g" -e "s/@KSERVICE/$SERVICE_NAME/g" | kubectl delete -f-
