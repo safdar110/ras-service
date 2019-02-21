409f7a5a1258e984664071993bdcacc6254e7120
Khawar <khawarhere@gmail.com>
2019-02-21 21:42:47 +0500

    ssd

--- src/main/java/com/appiskey/raservice/controller/DepartmentController.java
+++ src/main/java/com/appiskey/raservice/controller/DepartmentController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -19,7 +20,7 @@ import java.util.UUID;
 public class DepartmentController extends BaseController<DepartmentService, Department> {
 
     @PostMapping("/search")
-    public Iterable<Department> searchByName(@RequestBody Map<String, String> body) {
+    public List<Department> searchByName(@RequestBody Map<String, String> body) {
         return service.searchByDepartmentName(body.get("keyword"));
     }
 }
--- src/main/java/com/appiskey/raservice/service/DepartmentService.java
+++ src/main/java/com/appiskey/raservice/service/DepartmentService.java
@@ -4,6 +4,7 @@ import com.appiskey.raservice.model.Department;
 import com.appiskey.raservice.model.Skill;
 import org.springframework.http.ResponseEntity;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -11,5 +12,5 @@ import java.util.UUID;
  * Created by appiskey on 2/4/19.
  */
 public interface DepartmentService extends BaseService<Department>{
-    public Iterable<Department> searchByDepartmentName(String keyword);
+    public List<Department> searchByDepartmentName(String keyword);
 }
